f52b955e41d74cd33d25264367d053e12f8536f6
Alex Min <alexmin@2424@yahoo.com>
2022-04-03 23:13:44 -0700

    fixed before phase 1 frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,17 +8,16 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
 
