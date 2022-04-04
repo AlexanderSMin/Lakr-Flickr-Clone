941aff4c365b7797caf49b0eb20632a00ba235d5
Alex Min <alexmin@2424@yahoo.com>
2022-04-03 23:32:51 -0700

    login form page

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
-
 import './index.css';
-
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-
-
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -19,6 +15,7 @@ if (process.env.NODE_ENV !== "production") {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
