1327b1e66f1df3cf702c2f152b3cc37270552237
Alex Min <alexmin@2424@yahoo.com>
2022-04-10 21:05:04 -0700

    favicon done

---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,14 @@
+import './HomePage.css';
+
+const HomePage = () => {
+    return (
+        <>
+        <div className="home_page">
+            <p className="lakr_title">Lakr </p>
+            <br />
+            <p className="lakr_message">Bleed Purple and Gold. Join the Lakr Community</p>
+        </div>
+        </>
+   )
+};
+export default HomePage;
